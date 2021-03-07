a402b7f238ddbbe62369971c895ca97296b0c3e6
asanoH <hiroki.asano.bigmoon@gmail.com>
2021-03-07 17:49:58 +0900

    testing

--- default.nix
+++ default.nix
@@ -13,6 +13,7 @@ let
             hlint
             p.ihp
             mmark
+            hspec
         ];
         otherDeps = p: with p; [
             # Native dependencies, e.g. imagemagick
--- test/Web/Controller/PostsSpec.hs
+++ test/Web/Controller/PostsSpec.hs
@@ -4,16 +4,31 @@ import           Test.Hspec
 import           IHP.Test.Mocking
 import           IHP.FrameworkConfig (ConfigBuilder(..))
 import           IHP.Prelude
-import           IHP.QueryBuilder (fetch, query)
+import           IHP.QueryBuilder (query)
+import           Web.Controller.Prelude (fetch)
 
 import           Web.Types
 import           Web.Routes
 import           Generated.Types
 import           Main ()
+import           Config (config)
+import           Data.ByteString.Lazy.UTF8 (toString)
+import           Network.HTTP.Types (status200)
+import           Data.Text (unpack)
 
 spec :: Spec
 spec = beforeAll (pure config >>= mockContext WebApplication) do
-  describe "User controller" $ do
-    it "has no existing users" $ withContext do
+  describe "Post controller" $ do
+    it "has existing posts" $ withContext do
       posts <- query @Post |> fetch
-      posts `shouldBe` []
+      posts `shouldNotBe` []
+    it "responds with some content" $ withContext do
+      content <- mockActionResponse NewPostAction
+      (toString content) `shouldContain` "<!DOCTYPE HTML>"
+    it "creates a new post" $ withParams [("a-test-param","some-value")] do
+      --content <- mockActionResponse NewPostAction
+      --putStrLn $ "test " <> (tshow content)
+      mockActionStatus CreatePostAction `shouldReturn` status200
+    it "returns a redirect header" $ withContext do
+      hs <- headers (mockAction NewPostAction)
+      (unpack $ tshow hs) `shouldContain` "Content-Type"
