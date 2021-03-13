69b93a2f0a7757affef1ea6260963d1f5ebd09d4
asanoH <hiroki.asano.bigmoon@gmail.com>
2021-03-13 17:49:55 +0900

    Jobs

--- Main.hs
+++ Main.hs
@@ -10,6 +10,13 @@ import Web.FrontController
 import Web.Types
 import Web.Worker
 
+import qualified Control.Concurrent.Async as Async
+import qualified IHP.Job.Runner as Job
+import IHP.ScriptSupport
+import IHP.ModelSupport
+import IHP.MailPrelude
+import IHP.Log.Types (Logger)
+
 instance FrontController RootApplication where
     controllers = [
             mountFrontController WebApplication
@@ -19,4 +26,14 @@ instance Worker RootApplication where
     workers _ = []
 
 main :: IO ()
-main = IHP.Server.run config
+main = do
+    --runScript config $ Job.runJobWorkers $ workers WebApplication
+    frameworkConfig <- buildFrameworkConfig config
+    FrameworkConfig { environment, dbPoolIdleTime, dbPoolMaxConnections, databaseUrl, logger } <- buildFrameworkConfig config
+    modelContext1 <- createModelContext dbPoolIdleTime dbPoolMaxConnections databaseUrl logger
+    Async.withAsync (do
+        let ?context = frameworkConfig 
+        let ?modelContext = modelContext1
+        Job.runJobWorkers $ workers WebApplication) (\_ -> IHP.Server.run config)
+    --IHP.Server.run config
+
